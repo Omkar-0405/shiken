e5cb7bc775e2a9d1a369e7fc70c35c85048a25cf
krish10k <krushna.dahake.01@gmail.com>
2021-09-27 23:59:13 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './app';
+import App from './App';
 import { StoreProvider } from './context/context';
 
 ReactDOM.render(
